1bd3adce66f262684c94baf82bab5d2858860542
Brian Sandro <crurider@gmail.com>
2022-09-28 12:03:37 +0200

    Fixed all bugs, now showing all SSIDs with passwds

--- getWifiPasswd.py
+++ getWifiPasswd.py
@@ -1,6 +1,37 @@
 import subprocess
 
-interfaceInfo = subprocess.call(["netsh", "wlan", "show", "interfaces"]).split("\n")
-networkName = [line.split(':')[0] for line in interfaceInfo if "Profile" in line]
+# helper method
+def clean_output(strng):
+    strng = strng.replace(":", "").strip()
+    return strng[:strng.index("\\")]
 
-print(networkName)
+# cmd command: "netsh wlan show profiles" 
+wlan_data = str(subprocess.check_output(["netsh", "wlan", "show", "profiles"])).split("\n")
+
+# cleaning
+for i in wlan_data:
+    profiles = i.split("All User Profile")
+for i in profiles:
+    if "User profiles" in i:
+        profiles.remove(i)
+for i in range(len(profiles)):
+    profiles[i] = clean_output(profiles[i])
+
+# cmd command: "netsh wlan show profile NETWORK key=clear"
+results = []
+for i in profiles:
+    profile_info = (str(subprocess.check_output(["netsh", "wlan", "show", "profile", i, "key=clear"])))
+    if "Key Content" in profile_info:
+        results.append(profile_info)
+
+# cleaning
+results_clean = []
+for i in results:
+    spl = i.split("Key Content")
+    spl[0] = spl[0][spl[0].find(" "):spl[0].find("on interface")].strip()
+    spl[1] = clean_output(spl[1])
+    temp = "Network: " + spl[0] + ", Password: " + spl[1]
+    results_clean.append(temp)
+
+for i in results_clean:
+    print(i)
